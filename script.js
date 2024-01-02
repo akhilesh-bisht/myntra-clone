4c96fe74536a8a9e8dd45cf61ce82f8513ec9387
akhilesh <akhileshbisht2020@gmail.com~>
2024-01-02 23:14:42 +0530

    filtering

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let bagItem = [];
 onLoad();
+let searchInput = document.querySelector(".search_input");
+searchInput.addEventListener("input", filterHandler);
+
+function filterHandler(event) {
+  let searchText = event.target.value.toLowerCase();
+  let filterItem = filterItemsByCompany(searchText);
+  displayhomepage(filterItem);
+}
 
 function onLoad() {
   let bagItemStr = localStorage.getItem("bagItem");
@@ -7,6 +15,7 @@ function onLoad() {
   displayhomepage();
   displaybgCount();
 }
+
 function addtobag(itemid) {
   bagItem.push(itemid);
   localStorage.setItem("bagItem", JSON.stringify(bagItem));
@@ -14,35 +23,39 @@ function addtobag(itemid) {
 }
 
 function displaybgCount() {
-  let bagItemcount = document.querySelector(".bag-item-count");
-  if (bagItem.length > 0) {
-    bagItemcount.style.visibility = "visible";
-    bagItemcount.innerText = bagItem.length;
-  } else {
-    bagItemcount.style.visibility = "hidden";
-  }
+  let bagItemCount = document.querySelector(".bag-item-count");
+  bagItemCount.style.visibility = bagItem.length > 0 ? "visible" : "hidden";
+  bagItemCount.innerText = bagItem.length;
 }
-function displayhomepage() {
+
+function displayhomepage(filteredItems = items) {
   let itemContainerElement = document.querySelector(".items-container");
 
   if (!itemContainerElement) {
     return;
   }
+
   let innerHTML = "";
-  items.forEach((item) => {
+  filteredItems.forEach((item) => {
     innerHTML += `<div class="item-container">
         <img class="item-imgge" src="${item.image}" alt="">
         <div class="rating">${item.rating.stars}|${item.rating.count}</div>
         <div class="company-name">${item.company}</div>
         <div class="item-name">${item.item_name}</div>
         <div class="price">
-    <span class="current-price"> rs ${item.current_price} </span>
-    <span class="original-price">rs  ${item.original_price} </span> 
-     <span class="discount"> (${item.discount_percentage} % off )</span>
-     </div>
-<button class="btn-add-bag" onclick="addtobag(${item.id})" >add to Bag</button>
-</div>`;
+          <span class="current-price"> rs ${item.current_price} </span>
+          <span class="original-price">rs  ${item.original_price} </span>
+          <span class="discount"> (${item.discount_percentage} % off )</span>
+        </div>
+        <button class="btn-add-bag" onclick="addtobag(${item.id})">Add to Bag</button>
+      </div>`;
   });
 
   itemContainerElement.innerHTML = innerHTML;
+}
+
+function filterItemsByCompany(searchText) {
+  return items.filter((item) => {
+    return item.company.toLowerCase().includes(searchText);
+  });
 }
